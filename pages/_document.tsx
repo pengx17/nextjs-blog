ab42ff11bf2d288ca74057296c4574645b8ecb26
Peng Xiao <pengxiao@freewheel.tv>
2021-08-05 14:41:25 +0800

    fix: add font file to document

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+          <link
+            href="https://cdn.jsdelivr.net/gh/pengx17/source-fonts@0.0.3/fonts.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
