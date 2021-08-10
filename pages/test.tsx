0fe146b8d1c8c24f3cc58a7c284687960f9de798
Peng Xiao <pengxiao@freewheel.tv>
2021-08-10 19:11:43 +0800

    up

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import useSWR from "swr";
 import Layout from "../components/layout";
@@ -9,6 +10,131 @@ const fetcher = (url) =>
     }
     return res.json();
   });
+
+const example = {
+  url: "https://discord.com/channels/@me",
+  title: "Discord - A New Way to Chat with Friends & Communities",
+  siteName: "Discord",
+  description:
+    "Discord is the easiest way to communicate over voice, video, and text.  Chat, hang out, and stay close with your friends and communities.",
+  mediaType: "website",
+  contentType: "text/html",
+  images: ["https://discord.com/assets/ee7c382d9257652a88c8f7b7f22a994d.png"],
+  videos: [],
+  favicons: ["https://discord.com/assets/847541504914fd33810e70a0ea73177e.ico"],
+};
+
+// Credits: taken directly from innos.io
+const Preview = ({ data }: { data: typeof example }) => {
+  return (
+    <>
+      <div className="root">
+        <div className="card-container">
+          <div className="text-container">
+            <div className="text-container-title">{data.title}</div>
+            <div className="text-container-description">{data.description}</div>
+            <div className="text-container-url-container">
+              <img src={data.favicons[0]} width={16} height={16} alt="favico" />
+              <span className="text-container-url">{data.url}</span>
+            </div>
+          </div>
+          <div className="cover-container">
+            {data.images[0] && (
+              <img className="cover-image" src={data.images[0]} alt="cover" />
+            )}
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .root {
+          width: 720px;
+          display: block;
+          cursor: pointer;
+          user-select: none;
+          border-radius: 6px;
+          border: 1px solid #dee0e3;
+          overflow: hidden;
+          border: 1px solid #dee0e3;
+        }
+        .card-container {
+          width: 100%;
+          max-height: 120px;
+          background-color: #f8f8f8;
+          display: flex;
+          justify-content: space-between;
+          align-items: stretch;
+        }
+        .text-container {
+          padding: 12px 16px;
+          flex-shrink: 2;
+          overflow: hidden;
+        }
+        .cover-container {
+          width: 33.3%;
+          min-width: 33.3%;
+          flex-shrink: 1;
+          flex-grow: 0;
+        }
+        .text-container-title {
+          font-size: 16px;
+          font-weight: 500;
+          line-height: 26px;
+          color: #1f2329;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+        .text-container-description {
+          max-height: 40px;
+          line-height: 20px;
+          font-size: 14px;
+          font-weight: 400;
+          color: #646a73;
+          margin-top: 6px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: break-spaces;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+        }
+        .text-container-url-container {
+          display: flex;
+          align-items: center;
+          flex-wrap: nowrap;
+          flex-grow: 1;
+          flex-shrink: 2;
+          min-width: 0;
+          flex-direction: row;
+          height: 17px;
+          line-height: 17px;
+          font-size: 12px;
+          color: #1f2329;
+          margin-top: 6px;
+        }
+        .text-container-url {
+          margin-left: 8px;
+          flex-grow: 0;
+          flex-shrink: 1;
+          min-width: 0;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+        .cover-image {
+          object-position: 50% 50%;
+          background-size: auto 100%;
+          background-position-y: 50%;
+          background-repeat: no-repeat;
+          object-fit: cover;
+          overflow: hidden;
+          width: 100%;
+          height: 100%;
+        }
+      `}</style>
+    </>
+  );
+};
 
 export default function About() {
   const [url, setUrl] = useState("");
@@ -28,7 +154,7 @@ export default function About() {
           <strong>{error}</strong>
         </div>
 
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        {data && <Preview data={data} />}
       </Layout>
       <style jsx>{`
         .input {
