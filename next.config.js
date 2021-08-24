f1c2bc3bd701941df95b8be385c525720180adcf
Peng Xiao <pengxiao@outlook.com>
2021-08-24 22:56:23 +0800

    chore: update

--- next.config.js
+++ next.config.js
@@ -2,25 +2,4 @@ const withMDX = require("@next/mdx")();
 
 module.exports = withMDX({
   basePath: process.env.BASE_PATH ?? "",
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/preview-link",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 });
--- pages/api/link-preview.ts
+++ pages/api/link-preview.ts
@@ -1,14 +1,39 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getLinkPreview } from "link-preview-js";
 
-export default async function linkPreviewHandler(req, res) {
+const headers = [
+  { key: "Access-Control-Allow-Credentials", value: "true" },
+  { key: "Access-Control-Allow-Origin", value: "*" },
+  {
+    key: "Access-Control-Allow-Methods",
+    value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  },
+  {
+    key: "Access-Control-Allow-Headers",
+    value:
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  },
+];
+
+// See https://vercel.com/docs/serverless-functions/introduction
+export default async function linkPreviewHandler(
+  req: VercelRequest,
+  res: VercelResponse
+) {
   const { url } = req.query;
   console.info("fetching " + url);
-  const data = await getLinkPreview(url, {
-    timeout: 10000,
-    imagesPropertyType: "og",
-    headers: {
-      "user-agent": "googlebot",
-    },
-  });
-  res.json(data);
+  try {
+    const data = await getLinkPreview(Array.isArray(url) ? url[0] : url, {
+      timeout: 3000,
+      headers: {
+        "user-agent": "googlebot",
+      },
+    });
+    headers.forEach(({ key, value }) => {
+      res.setHeader(key, value);
+    });
+    res.json(data);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 }
