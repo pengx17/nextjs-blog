d41192ae1ce42c74668d94b48765acab4a3318b3
Peng Xiao <pengxiao@outlook.com>
2021-11-25 01:38:26 +0800

    fix: build

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import Layout from "../components/layout";
+import { Layout } from "../components/layout";
 import LinkPreview from "../components/link-preview";
 
 export default function About() {
