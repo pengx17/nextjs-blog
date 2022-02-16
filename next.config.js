1b2cd4cd9fa508c9e0e738351ab1044715f0f478
Peng Xiao <pengxiao@outlook.com>
2022-02-16 10:11:39 +0800

    feat: pwa support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
+const prod = process.env.NODE_ENV === "production";
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -10,4 +15,11 @@ const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([withPWA], {
+  ...nextConfig,
+  pwa: {
+    dest: "public",
+    disable: !prod,
+    skipWaiting: true,
+  },
+});
